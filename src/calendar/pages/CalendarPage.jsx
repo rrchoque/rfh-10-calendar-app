a02d9c842adc23ac385ada631bb51a86eaddc6eb
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-10-13 00:45:16 -0400

    Se configuró la captura de los eventos click, doubleclick y el ultimo formato de vista seleccionado

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -4,6 +4,7 @@ import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { CalendarEvent, Navbar } from "../"
 import { addHours } from 'date-fns'
 import { getMessagesES, localizer } from '../../helpers'
+import { useState } from 'react'
 
 
 
@@ -22,6 +23,8 @@ const events = [{
 
 export const CalendarPage = () => {
 
+  const [ lastView, setLastView ] = useState(localStorage.getItem('lastView') || 'week' );
+
   const eventStyleGetter = ( event, start, end, isSelected ) => {
 
     const style = {
@@ -36,6 +39,20 @@ export const CalendarPage = () => {
     }
   }
 
+  const onDoubleClick = ( event ) => {
+    console.log({ doubleClick: event });
+  }
+
+  const onSelect = ( event ) => {
+    console.log({ click: event });
+  }
+
+  const onViewChanged = ( event ) => {
+    localStorage.setItem('lastView', event );
+    setLastView( event )
+  }
+
+
   return (
     <>
       <Navbar />
@@ -44,6 +61,7 @@ export const CalendarPage = () => {
         culture='es'
         localizer={localizer}
         events={events}
+        defaultView={ lastView }
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc(100vh - 80px)' }}
@@ -52,6 +70,9 @@ export const CalendarPage = () => {
         components={{
           event: CalendarEvent
         }}
+        onDoubleClickEvent={ onDoubleClick }
+        onSelectEvent={ onSelect }
+        onView={ onViewChanged }
       />
     </>
   )
