e03b896a7d7022854f7924f14522fe7f46934b9f
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-10-13 00:27:32 -0400

    Se configuró el calendario con mensajes en español

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,55 @@
+import { Calendar } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
 import { Navbar } from "../"
+import { addHours } from 'date-fns'
+import { getMessagesES, localizer } from '../../helpers'
+
+
+
+const events = [{
+  title: 'Cumpleaños del jefe',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours( new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Reynaldo'
+  }
+
+}]
 
 export const CalendarPage = () => {
+
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return {
+      style
+    }
+  }
+
   return (
     <>
       <Navbar />
+
+      <Calendar
+        culture='es'
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+        messages={getMessagesES()}
+        eventPropGetter={ eventStyleGetter }
+      />
     </>
   )
 }
