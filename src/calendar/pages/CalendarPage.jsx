d3a9d205d148ce6c40fb2a9e8ec4b04098c1b59f
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-10-17 16:15:04 -0400

    Se creó la funcionalidad de abrir el modal para registrar un nuevo evento

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,7 +1,7 @@
 import { Calendar } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
-import { CalendarEvent, CalendarModal, Navbar } from "../"
+import { CalendarEvent, CalendarModal, FabAddNew, Navbar } from "../"
 import { addHours } from 'date-fns'
 import { getMessagesES, localizer } from '../../helpers'
 import { useState } from 'react'
@@ -67,6 +67,7 @@ export const CalendarPage = () => {
       />
 
       <CalendarModal />
+      <FabAddNew />
     </>
   )
 }
