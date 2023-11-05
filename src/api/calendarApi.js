de995b993aa76d70d99cf42b8bcdaf0bbb3345ef
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-11-05 00:25:20 -0400

    Se agregó la permanencia en la página mientras el token esté vigente, y la renovación automática

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -8,6 +8,15 @@ const calendarApi = axios.create({
 });
 
 // Todo: configurar interceptores
+calendarApi.interceptors.request.use( config => {
+
+    config.headers = {
+        ...config.headers,
+        'x-token': localStorage.getItem('token')
+    }
+
+    return config;
+})
 
 export default calendarApi;
 
--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -40,6 +40,21 @@ export const useAuthStore = () => {
         }
     }
 
+    const checkAuthToken = async() => {
+        const token = localStorage.getItem('token');
+        if ( !token ) return dispatch( onLogout() );
+
+        try {
+            const { data } = await calendarApi.get('auth/renew');
+            localStorage.setItem('token', data.token );
+            localStorage.setItem('token-init-date', new Date().getTime() );
+            dispatch( onLogin({ name: data.name, uid: data.uid }) );
+        } catch (error) {
+            localStorage.clear();
+            dispatch( onLogout() );
+        }
+    }
+
     return {
         //* Propiedades
         errorMessage,
@@ -49,6 +64,7 @@ export const useAuthStore = () => {
         //* Métodos
         startLogin,
         startRegister,
+        checkAuthToken,
     }
 
 }
